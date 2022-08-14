9d0bc04ec882aa270619d3032ec9f77b405fb070
Mateusz Grden <matgrden@gmail.com>
2022-08-14 18:56:35 +0200

    challenges component changed

--- src/Components/Challenges/Challenges.tsx
+++ src/Components/Challenges/Challenges.tsx
@@ -1,24 +1,35 @@
 import { useEffect } from "react";
 import ProtectedRoute from "../ProtectedRoute/ProtectedRoute";
-import { fetchChallenges } from "src/Redux/Slices/challengesSlice";
+import { fetchChallenges, setStatus } from "src/Redux/Slices/challengesSlice";
 import { useAppDispatch, useAppSelector } from "src/Redux/Hooks/Hooks";
 
-import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import ButtonBase from "@mui/material/ButtonBase";
-
-const Img = styled("img")({
-  margin: "auto",
-  display: "block",
-  maxWidth: "100%",
-  maxHeight: "100%",
-});
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
 
 export const Challenges = () => {
   const dispatch = useAppDispatch();
+
+  const checkDate = () => {
+    let date = new Date();
+    if (date.getDay() === 1 && date.getHours() === 18) {
+      console.log("HELLO WORLD!");
+      dispatch(setStatus("idle"));
+    }
+  };
+  // setInterval(checkDate, 5000);
+
+  // const dateLoop = setInterval(() => {
+  //   checkDate();
+  // }, 5000);
+
+  const getMultipleRandom = (arr: Array<{}>, num: number) => {
+    const shuffled = [...arr].sort(() => 0.5 - Math.random());
+    return shuffled.slice(0, num);
+  };
 
   const challengesStatus: string = useAppSelector(
     (state) => state.challenges.status
@@ -32,164 +43,42 @@ export const Challenges = () => {
     }
   }, [challengesStatus, dispatch]);
 
-  const slicedArray = challengesData.slice(0, 2);
-  console.log(slicedArray);
-
   return (
     <ProtectedRoute>
-      <Box sx={{ flexGrow: 1 }}>
-        <Grid
-          container
-          spacing={3}
-          sx={{ flexDirection: { xs: "column", lg: "row" } }}
-        >
-          <Grid item xs>
-            <Paper
-              sx={{
-                p: 2,
-                margin: "auto",
-                maxWidth: 500,
-                flexGrow: 1,
-                backgroundColor: (theme) =>
-                  theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-              }}
-            >
-              <Grid container spacing={2}>
-                <Grid item>
-                  <ButtonBase sx={{ width: 128, height: 128 }}>
-                    <Img alt="complex" src="/static/images/grid/complex.jpg" />
-                  </ButtonBase>
-                </Grid>
-                <Grid item xs={12} sm container>
-                  <Grid item xs container direction="column" spacing={2}>
-                    <Grid item xs>
+      <Box p={5} minHeight="100vh">
+        <Grid container spacing={5} sx={{ justifyContent: "center" }}>
+          {challengesData &&
+            getMultipleRandom(challengesData, 3).map((challenge: any) => {
+              return (
+                <Grid key={challenge.id} item>
+                  <Card sx={{ width: { xs: 250, lg: 400 }, height: 275 }}>
+                    <CardMedia
+                      sx={{ height: 140 }}
+                      image={challenge.challenge_url}
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        {challenge.title}
+                      </Typography>
                       <Typography
-                        gutterBottom
-                        variant="subtitle1"
-                        component="div"
+                        variant="body2"
+                        color="textSecondary"
+                        component="p"
                       >
-                        Standard license
+                        {challenge.description}
                       </Typography>
-                      <Typography variant="body2" gutterBottom>
-                        Full resolution 1920x1080 • JPEG
-                      </Typography>
-                      <Typography variant="body2" color="text.secondary">
-                        ID: 1030114
-                      </Typography>
-                    </Grid>
-                    <Grid item>
-                      <Typography sx={{ cursor: "pointer" }} variant="body2">
-                        Remove
-                      </Typography>
-                    </Grid>
-                  </Grid>
-                  <Grid item>
-                    <Typography variant="subtitle1" component="div">
-                      $19.00
-                    </Typography>
-                  </Grid>
-                </Grid>
-              </Grid>
-            </Paper>
-          </Grid>
-          <Grid item xs>
-            <Paper
-              sx={{
-                p: 2,
-                margin: "auto",
-                maxWidth: 500,
-                flexGrow: 1,
-                backgroundColor: (theme) =>
-                  theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-              }}
-            >
-              <Grid container spacing={2}>
-                <Grid item>
-                  <ButtonBase sx={{ width: 128, height: 128 }}>
-                    <Img alt="complex" src="/static/images/grid/complex.jpg" />
-                  </ButtonBase>
-                </Grid>
-                <Grid item xs={12} sm container>
-                  <Grid item xs container direction="column" spacing={2}>
-                    <Grid item xs>
                       <Typography
-                        gutterBottom
-                        variant="subtitle1"
-                        component="div"
+                        variant="overline"
+                        component="p"
+                        sx={{ textAlign: "right" }}
                       >
-                        Standard license
+                        {challenge.level}
                       </Typography>
-                      <Typography variant="body2" gutterBottom>
-                        Full resolution 1920x1080 • JPEG
-                      </Typography>
-                      <Typography variant="body2" color="text.secondary">
-                        ID: 1030114
-                      </Typography>
-                    </Grid>
-                    <Grid item>
-                      <Typography sx={{ cursor: "pointer" }} variant="body2">
-                        Remove
-                      </Typography>
-                    </Grid>
-                  </Grid>
-                  <Grid item>
-                    <Typography variant="subtitle1" component="div">
-                      $19.00
-                    </Typography>
-                  </Grid>
+                    </CardContent>
+                  </Card>
                 </Grid>
-              </Grid>
-            </Paper>
-          </Grid>
-          <Grid item xs>
-            <Paper
-              sx={{
-                p: 2,
-                margin: "auto",
-                maxWidth: 500,
-                flexGrow: 1,
-                backgroundColor: (theme) =>
-                  theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-              }}
-            >
-              <Grid container spacing={2}>
-                <Grid item>
-                  <ButtonBase sx={{ width: 128, height: 128 }}>
-                    <Img alt="complex" src="/static/images/grid/complex.jpg" />
-                  </ButtonBase>
-                </Grid>
-                <Grid item xs={12} sm container>
-                  <Grid item xs container direction="column" spacing={2}>
-                    <Grid item xs>
-                      <Typography
-                        gutterBottom
-                        variant="subtitle1"
-                        component="div"
-                      >
-                        Standard license
-                      </Typography>
-                      <Typography variant="body2" gutterBottom>
-                        Full resolution 1920x1080 • JPEG
-                      </Typography>
-                      <Typography variant="body2" color="text.secondary">
-                        ID: 1030114
-                      </Typography>
-                    </Grid>
-                    <Grid item>
-                      <Typography sx={{ cursor: "pointer" }} variant="body2">
-                        Remove
-                      </Typography>
-                    </Grid>
-                  </Grid>
-                  <Grid item>
-                    <Typography variant="subtitle1" component="div">
-                      $19.00
-                    </Typography>
-                  </Grid>
-                </Grid>
-              </Grid>
-            </Paper>
-          </Grid>
+              );
+            })}
         </Grid>
       </Box>
     </ProtectedRoute>

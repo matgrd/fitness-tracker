1f0c459d719fbe7267cf3197627e8bc9096562aa
Mateusz Grden <matgrden@gmail.com>
2022-08-18 23:38:58 +0200

    the user has individual challenges added

--- src/Components/Challenges/Challenges.tsx
+++ src/Components/Challenges/Challenges.tsx
@@ -1,7 +1,8 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ProtectedRoute from "../ProtectedRoute/ProtectedRoute";
-import { fetchChallenges, setStatus } from "src/Redux/Slices/challengesSlice";
+import { fetchChallenges } from "src/Redux/Slices/challengesSlice";
 import { useAppDispatch, useAppSelector } from "src/Redux/Hooks/Hooks";
+import { supabase } from "src/supabaseClient";
 
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
@@ -12,44 +13,84 @@ import CardMedia from "@mui/material/CardMedia";
 
 export const Challenges = () => {
   const dispatch = useAppDispatch();
-
-  const checkDate = () => {
-    let date = new Date();
-    if (date.getDay() === 1 && date.getHours() === 18) {
-      console.log("HELLO WORLD!");
-      dispatch(setStatus("idle"));
-    }
-  };
-  // setInterval(checkDate, 5000);
-
-  // const dateLoop = setInterval(() => {
-  //   checkDate();
-  // }, 5000);
-
-  const getMultipleRandom = (arr: Array<{}>, num: number) => {
-    const shuffled = [...arr].sort(() => 0.5 - Math.random());
-    return shuffled.slice(0, num);
-  };
+  const currentUser: any = supabase.auth.user();
+  const [userChallenges, setUserChallenges] = useState<any>();
 
   const challengesStatus: string = useAppSelector(
     (state) => state.challenges.status
   );
   const challengesData: [] = useAppSelector((state) => state.challenges.data);
 
+  const getMultipleRandom = async (arr: Array<{}>, num: number) => {
+    const shuffled = [...arr].sort(() => 0.5 - Math.random());
+    const chosenChallenges = await shuffled.slice(0, num);
+    return chosenChallenges;
+  };
+
+  const addChallengesToUser = async () => {
+    const { data: haveUserChallenges }: any = await supabase
+      .from("userChallenges")
+      .select("user")
+      .eq("user", currentUser.id)
+      .single();
+
+    if (!haveUserChallenges) {
+      const userChosenChallenges = await getMultipleRandom(challengesData, 3);
+      if (userChosenChallenges) {
+        const updates = {
+          user: currentUser.id,
+          first_challenge: userChosenChallenges[0],
+          second_challenge: userChosenChallenges[1],
+          third_challenge: userChosenChallenges[2],
+          updated_at: new Date(),
+        };
+
+        let { error } = await supabase
+          .from("userChallenges")
+          .upsert(updates, { returning: "minimal" });
+      }
+    }
+    const fetchUserChallenges = async () => {
+      let { data: userChallenges } = await supabase
+        .from("userChallenges")
+        .select(`first_challenge, second_challenge, third_challenge`)
+        .eq("user", currentUser.id)
+        .single();
+      setUserChallenges(userChallenges);
+    };
+    fetchUserChallenges();
+  };
+
   useEffect(() => {
     if (challengesStatus === "idle") {
       dispatch(fetchChallenges());
     }
+    if (challengesStatus === "succeeded") {
+      addChallengesToUser();
+    }
   }, [challengesStatus, dispatch]);
+
+  // const checkDate = () => {
+  //   let date = new Date();
+  //   if (date.getDay() === 1 && date.getHours() === 18) {
+  //     console.log("HELLO WORLD!");
+  //     dispatch(setStatus("idle"));
+  //   }
+  // };
+  // setInterval(checkDate, 5000);
+
+  // const dateLoop = setInterval(() => {
+  //   checkDate();
+  // }, 5000);
 
   return (
     <ProtectedRoute>
       <Box p={5} minHeight="100vh">
         <Grid container spacing={5} sx={{ justifyContent: "center" }}>
-          {challengesData &&
-            getMultipleRandom(challengesData, 3).map((challenge: any) => {
+          {userChallenges &&
+            Object.keys(userChallenges).map((keyName: any) => {
               return (
-                <Grid key={challenge.id} item>
+                <Grid key={userChallenges[keyName].id} item>
                   <Card
                     sx={{
                       width: { xs: 250, lg: 400 },
@@ -58,25 +99,25 @@ export const Challenges = () => {
                   >
                     <CardMedia
                       sx={{ height: { xs: 140, lg: 250 } }}
-                      image={challenge.challenge_url}
+                      image={userChallenges[keyName].challenge_url}
                     />
                     <CardContent>
                       <Typography gutterBottom variant="h5" component="h2">
-                        {challenge.title}
+                        {userChallenges[keyName].title}
                       </Typography>
                       <Typography
                         variant="body2"
                         color="textSecondary"
                         component="p"
                       >
-                        {challenge.description}
+                        {userChallenges[keyName].description}
                       </Typography>
                       <Typography
                         variant="overline"
                         component="p"
                         sx={{ textAlign: "right" }}
                       >
-                        {challenge.level}
+                        {userChallenges[keyName].level}
                       </Typography>
                     </CardContent>
                   </Card>
